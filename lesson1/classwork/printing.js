b665a25354b0b5c978be4ebf870b3b7e4d6202ab
Kaya-12345 <purpledog13131@gmail.com>
2026-03-22 17:40:36 -0700

    Update printing.js
    Added console logs for greeting and personal information.

---
+++ lesson1/classwork/printing.js
@@ -0,0 +1,6 @@
+console.log("Hello,world!");
+console.log("My name is xxx.");
+console.log('My favorite food is xxx.')
+
+// Error: quatation marks mismatched
+// console.log('This dosent work");         
