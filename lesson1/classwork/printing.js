7cf2acde9bd5f20eb6ae9d3d79b534ee07159da4
Kaya-12345 <purpledog13131@gmail.com>
2026-03-22 17:41:34 -0700

    Update printing.js
    Fixed quotation marks in the console log statements.

--- lesson1/classwork/printing.js
+++ lesson1/classwork/printing.js
@@ -2,5 +2,4 @@ console.log("Hello,world!");
 console.log("My name is xxx.");
 console.log('My favorite food is xxx.')
 
-// Error: quatation marks mismatched
-// console.log('This dosent work");         
+console.log('This dosent work");         
