9b13003d1bbc3aa11e523867a97b38ca64e231c5
Kaya-12345 <purpledog13131@gmail.com>
2026-04-05 17:15:15 -0700

    Update comparison_operators.js

---
+++ lesson3/classwork/comparison_operators.js
@@ -0,0 +1,5 @@
+// Comparison operators are used to compare two values and return a boolean (true or false)
+console.log(5 > 3); // Greater than
+console.log(5 < 3); // Less than
+console.log(5 >= 3); // Greater than or equal to
+console.log(5 <= 3); // Less than or equal to
