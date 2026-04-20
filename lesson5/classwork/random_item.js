4f037f6e2be4e30ee95f020da94bc2bc93a3527e
Kaya-12345 <purpledog13131@gmail.com>
2026-04-19 17:44:08 -0700

    Update random_item.js

---
+++ lesson5/classwork/random_item.js
@@ -0,0 +1,7 @@
+let animals = ["cat", "dog", "rabbit", "hamster", "parrot", "red panda", "monkey", "human"];
+
+let length = animals.length;
+
+let random_animal = Math.floor(Math.random() * length);
+let random_animal = animals[random_index];
+console.log("Random animal: ", random_animal)
